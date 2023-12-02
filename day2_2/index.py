ccda5cc9877d11d249edb570a05044edac4440e1
Sandrin Joy <sandrinejoy007@gmail.com>
2023-12-02 22:36:28 +0530

    day 2 part 2

---
+++ day2_2/index.py
@@ -0,0 +1,93 @@
+import os
+import math
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+text =  open_file(os.path.join(os.path.dirname(__file__), 'input.txt'))
+textConfig = {
+    'red': 12,
+    'green':13,
+    'blue' : 14,
+}
+arrFromText = text.split('\n')
+
+
+test = open_file(os.path.join(os.path.dirname(__file__), 'test.txt'))
+testConfig = {
+    'red': 12,
+    'green':13,
+    'blue' : 14,
+}
+arrFromTest = test.split('\n')
+testOutput = 8
+
+#struct
+# [gameId : [{red:, green, blue}, ...]]
+cubes = ['red', 'green', 'blue']
+def formatGame(item):
+    game = item.split(':')
+    gameId = game[0].strip().split(' ')[1]
+    rounds = game[1].split(';')
+    gameOp = []
+    for round in range(len(rounds)):
+        roundCubes = rounds[round].split(',')
+        roundConfig = {}
+        for k in range(len(roundCubes)):
+            cube = roundCubes[k]
+            cubeConfig = {}
+            cubeSet = cube.strip().split(' ')
+            
+            cubeConfig[cubeSet[1]] = cubeSet[0]
+            roundConfig[cubeSet[1]] = cubeSet[0]
+        gameOp.append(roundConfig)
+    return gameId, gameOp
+
+def getRGBFromRound(round):
+    if 'red' not in round.keys():
+        round['red'] = 0
+    if 'green' not in round.keys():
+        round['green'] = 0
+    if 'blue' not in round.keys():
+        round['blue'] = 0
+    return int(round['red']), int(round['green']), int(round['blue'])
+
+def getPowerOfGame(r, g, b):
+    return r*g*b
+
+def getSumPossibleGameIds(matches):
+    possibleGameIdSum = 0
+    for i in range(len(matches)):
+        matchId, games = formatGame(matches[i])
+        possibleMatch = True
+        for k in range(len(games)):
+            config = textConfig.copy()
+            round = games[k]
+            for cube in round.keys():
+                config[cube] = config[cube] - int(round[cube])
+            if config['red'] < 0 or config['green'] < 0 or config['blue'] < 0:
+                possibleMatch = False
+        if possibleMatch:
+            possibleGameIdSum += int(matchId)
+    return possibleGameIdSum
+
+def getSumPower(matches):
+    currentPower = 0
+    for i in range(len(matches)):
+        minR, minG, minB = 0,0,0
+        matchId, rounds = formatGame(matches[i])
+        for k in range(len(rounds)):
+            r, g, b = getRGBFromRound(rounds[k])
+            if r > minR:
+                minR = r
+            if g > minG:
+                minG = g
+            if b > minB:
+                minB = b
+        currentPower += getPowerOfGame(minR, minG, minB)
+    return currentPower
+
+print(getSumPower(arrFromText))
+
+
