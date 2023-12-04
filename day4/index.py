048576a43a68236ff0bd125c80d504ef321c128d
Sandrin Joy <sandrinejoy007@gmail.com>
2023-12-04 11:30:23 +0530

    day 4 part 2

--- day4/index.py
+++ day4/index.py
@@ -14,9 +14,12 @@ def formatInput(line):
     # winning:[]
     # numbers:[]
     # }}
+    
     game = line.split(':')
     gameId = game[0].strip()
-    id = gameId.split(' ')[1]
+    
+    id = gameId.strip().split('Card')[1].strip()
+    
     data =  game[1].strip().split('|')
     winning = list(filter(None,data[0].strip().split(' ')))   
     numbers = list(filter(None,data[1].strip().split(' ')))
@@ -34,11 +37,76 @@ def totalPointsWinning(text):
                     points = 1
                 else:
                     points *= 2
-        print(gameId, points)
+        
         total += points
     return total
+
+def makeMem(text):
+    mem = {}
+    textArr = text.split('\n')
+    for i in range(len(textArr)):
+
+        gameId, winning, numbers = formatInput(textArr[i]) 
+        
+        mem[int(gameId)] = {
+            'winning': winning,
+            'numbers': numbers
+        }
+    return mem
+def totalScratchCards(text):
+    total = 0
+    # this will contain the count of card copies received
+    mem = {}
+    dataMem = makeMem(text)
+    
+
+    textArr = text.split('\n')
+    for i in range(len(textArr)):
+        gameId, winning, numbers = formatInput(textArr[i]) 
+        copiesWon = 0
+        for j in range(len(winning)):
+            if winning[j] in numbers:
+                copiesWon += 1
+        for k in range(copiesWon):
+            copyId =int(gameId)+ k+1
+            if copyId in mem.keys():
+                mem[copyId] += 1
+            else:
+                mem[copyId] = 1
        
+        # now do for the copies of the same card
+        if int(gameId) in mem.keys():
+            memCopies = mem[int(gameId)]
+            for i in range(memCopies):
+                winning = dataMem[int(gameId)]['winning']
+                numbers = dataMem[int(gameId)]['numbers'] 
+                copiesWon = 0
+                for j in range(len(winning)):
+                    if winning[j] in numbers:
+                        copiesWon += 1
+                for k in range(copiesWon):
+                    copyId =int(gameId)+ k+1
+                    if copyId in mem.keys():
+                        mem[copyId] += 1
+                    else:
+                        mem[copyId] = 1
+
+
+    for key in dataMem.keys():
+        if key not in mem.keys():
+            mem[int(key)] = 1
+        else:
+            mem[int(key)] += 1
+    for key in mem.keys():
+        total += mem[int(key)]
+    print(total, mem)
+
+            
+    return total
+        
+        
 
 
 
 print(totalPointsWinning(text))
+print(totalScratchCards(text))
