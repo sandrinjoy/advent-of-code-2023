f7e87eaf25a6547bf159b44dc23d7a5c22b50d48
Sandrin Joy <sandrinejoy007@gmail.com>
2023-12-06 12:35:20 +0530

    day 6 p1

---
+++ day6/main.py
@@ -0,0 +1,23 @@
+import sys
+import math
+with open(sys.argv[1], 'r') as my_file:
+    F = my_file.read().strip().split('\n')
+    time= F[0].split(':')[1:][0].strip().split()
+    distance = F[1].split(':')[1:][0].strip().split()
+    waysList = []
+    for i, t in enumerate(time):
+        minD = int(distance[i])
+        T = int(t)
+        waysList.append(0)
+        for hold in range(T):
+            if hold == 0:
+                continue
+            remT = T - hold
+            remD = remT * hold
+            maxD =  remD
+            print(remT, maxD, hold, T)
+            if remD > minD:
+                waysList[i] += 1
+    print("p1 =>", math.prod(waysList))
+
+        
